3a1f0f7e03df96ff5866b8be73c4a3658be8ed82
zhh0907 <zhh0907@bu.edu>
2024-01-28 23:22:06 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -31,13 +31,44 @@ class Mul:
     
     def __repr__(self):
         if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
+            if isinstance(self.p2, (Add, Mul, Sub, Div)):
                  return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
+        if isinstance(self.p2, (Add, Mul, Sub, Div)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+class Div:
+    def __init__(self, p1, p2):
+        if p2 == 0 or (isinstance(p2, Int) and p2.i == 0):
+            raise Exception("Cannot divide by zero")
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, (Add, Mul, Sub, Div)):
+            if isinstance(self.p2, (Add, Mul)):
+                return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        if isinstance(self.p2, (Add, Mul, Sub, Div)):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " / " + repr(self.p2)
+    
+    
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+    def __repr__(self):
+        if isinstance(self.p1, (Add, Mul, Sub, Div)):
+            if isinstance(self.p2, (Add, Mul, Div)):
+                return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) - " + repr(self.p2)
+        if isinstance(self.p2, (Add, Mul, Sub, Div)):
+            return repr(self.p1) + " - ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " - " + repr(self.p2)
+
+
+poly = Add( Add( Int(4), Int(3)), Sub( X(), Mul( Int(1), Div( Mul(X(), X()), Int(1)))))
 print(poly)
